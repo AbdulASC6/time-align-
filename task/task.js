6010e77e6012d0cce31862cfdcf0801e3957011a
markASC6 <mwhittingham02@gmail.com>
2019-08-09 18:47:09 -0400

    Task page works with database

--- task/task.js
+++ task/task.js
@@ -1,33 +1,62 @@
 let database = firebase.database().ref()
-const usernameE = document.getElementById("username");
 const taskE = document.getElementById("task");
 const timeE = document.getElementById("time");
 const dayE = document.getElementById("day");
 const button = document.getElementById("submitButton");
+let count = 0
+let arr = []
 button.addEventListener("click",updateDB);
 
 function updateDB(event){
     event.preventDefault();
-    const username = usernameE.value;
     const task = taskE.value;
     const time = timeE.value;
     const day = dayE.value;
     
-    usernameE.value = "";
     taskE.value = "";
     timeE.value = "";
     dayE.value = "";
 
     let value = {
-        NAME: username,
         TASK: task,
         TIME: time,
         DAY: day
     }
+    arr.push(count)
+    count +=1
     database.push(value);
-    console.log(username + task)
+    console.log(arr)
 }
 
-// database.on("child_added", addMessag);
-// document.createElement("div")
-// let taskContainer = document.
+database.on("child_added", addTask);
+let taskContain = document.querySelector(".taskContain")
+
+function addTask(data){
+    const unit = data.val();
+    const task = unit.TASK;
+    const time = unit.TIME;
+    const day = unit.DAY;
+
+    let newUnit = document.createElement("div");
+    let newTask = document.createElement("p");
+    let newTime = document.createElement("p");
+    let newDay = document.createElement("p");
+
+    newTask.innerText = task;
+    newTime.innerText = time;
+    newDay.innerText = day;
+
+    taskContain.appendChild(newUnit);
+    newUnit.appendChild(newTask);
+    newUnit.appendChild(newTime);
+    newUnit.appendChild(newDay);
+
+    newUnit.className = "newUnit";
+    newTask.className = "newTask";
+    newTime.className = "newTime";
+    newDay.className = "newDay";
+
+
+    
+
+}
