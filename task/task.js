f76d2bc8e108e24fe32a746b61ce7b7568f64787
dnite2477@gmail.com <DavieASC6>
2019-08-09 16:11:28 -0400

    Fixed the database functionality

--- task/task.js
+++ task/task.js
@@ -2,7 +2,7 @@ let database = firebase.database().ref()
 const usernameE = document.getElementById("username");
 const taskE = document.getElementById("task");
 const timeE = document.getElementById("time");
-const dayE = document.getElementById("time");
+const dayE = document.getElementById("day");
 const button = document.getElementById("submitButton");
 button.addEventListener("click",updateDB);
 
@@ -10,5 +10,24 @@ function updateDB(event){
     event.preventDefault();
     const username = usernameE.value;
     const task = taskE.value;
+    const time = timeE.value;
+    const day = dayE.value;
+    
+    usernameE.value = "";
+    taskE.value = "";
+    timeE.value = "";
+    dayE.value = "";
+
+    let value = {
+        NAME: username,
+        TASK: task,
+        TIME: time,
+        DAY: day
+    }
+    database.push(value);
     console.log(username + task)
 }
+
+// database.on("child_added", addMessag);
+// document.createElement("div")
+// let taskContainer = document.
