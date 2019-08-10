58ec0cbc07f401ccfd1cd8b2b2bf2df86e88c051
markASC6 <mwhittingham02@gmail.com>
2019-08-10 11:22:24 -0400

    Worked on 'add task' button

--- task/task.js
+++ task/task.js
@@ -1,18 +1,52 @@
-let database = firebase.database().ref()
+let database = firebase.database().ref();
+const inputContain = document.querySelector(".inputContainer");
+const addTaskB = document.querySelector(".taskButton");
 const taskE = document.getElementById("task");
 const timeE = document.getElementById("time");
 const dayE = document.getElementById("day");
-const button = document.getElementById("submitButton");
+const submitTaskB = document.getElementById("submitButton");
+let postTime = new Date();
+let menu = false
 let count = 0
 let arr = []
-button.addEventListener("click",updateDB);
 
-function updateDB(event){
+inputContain.style.display = "none"
+submitTaskB.style.display = "none"
+addTaskB.innerText = "+"
+
+if (!menu) addTaskB.addEventListener("click", addTaskMenu); 
+if (menu) {
+    addTaskB.addEventListener("click", subTaskMenu);
+    console.log("bruh")
+}
+
+submitTaskB.addEventListener("click", updateDB);
+console.log(menu)
+function addTaskMenu(event) {
+    event.preventDefault();
+    inputContain.style.display = "inline";
+    submitTaskB.style.display = "block";
+    addTaskB.innerText = "-";
+    menu = true;
+    console.log(menu);
+}
+
+function subTaskMenu(event) {
+    //event.preventDefault();
+    inputContain.style.display = "none";
+    submitTaskB.style.display = "none";
+    addTaskB.innerText = "+";
+    menu = false;
+    console.log(menu);
+}
+
+function updateDB(event) {
     event.preventDefault();
     const task = taskE.value;
     const time = timeE.value;
     const day = dayE.value;
-    
+    let postTime = new Date();
+
     taskE.value = "";
     timeE.value = "";
     dayE.value = "";
@@ -20,22 +54,26 @@ function updateDB(event){
     let value = {
         TASK: task,
         TIME: time,
-        DAY: day
+        DAY: day,
+        POSTTIME: postTime
     }
     arr.push(count)
-    count +=1
-    database.push(value);
-    console.log(arr)
+    count += 1
+    
+    console.log(value);
+    database.push(value)
+    subTaskMenu();
 }
 
 database.on("child_added", addTask);
 let taskContain = document.querySelector(".taskContain")
 
-function addTask(data){
+function addTask(data) {
     const unit = data.val();
     const task = unit.TASK;
     const time = unit.TIME;
     const day = unit.DAY;
+
 
     let newUnit = document.createElement("div");
     let newTask = document.createElement("p");
@@ -52,11 +90,8 @@ function addTask(data){
     newUnit.appendChild(newDay);
 
     newUnit.className = "newUnit";
-    newTask.className = "newTask";
-    newTime.className = "newTime";
-    newDay.className = "newDay";
-
-
-    
+    newTask.className = "newTask new";
+    newTime.className = "newTime new";
+    newDay.className = "newDay new";
 
 }
